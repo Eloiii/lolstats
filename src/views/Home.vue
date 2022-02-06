89527b9581c3ee89bdf7d5c0d6e0490a13c393c5
Eloiii <eloi.charra@gmail.com>
2022-02-06 12:09:17 +0100

    Cleanup + basic info home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    summonerName : {{summonerName}}
+    <br/>
+    summonerId: {{summonerId}}
+    <br/>
+    accountId: {{accountId}}
+    <br/>
+    puuid: {{puuid}}
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        data: {},
+        api_key: "RGAPI-dc1b8fd3-f720-4f98-8e63-a7c7e5d3c938",
+        summonerName: "",
+        summonerId: "",
+        accountId: "",
+        puuid: ""
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+      }
+    },
+    methods: {
+      async fetchApi(link) {
+        const query = await fetch(link)
+        const response = await query.json()
+        return response
+      }
+    },
+    async created() {
+      this.summonerName = this.$route.params.summonerName
+      this.data = await this.fetchApi("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+this.summonerName+"?api_key="+this.api_key)
+      this.summonerId = this.data.id
+      this.accountId = this.data.accountId
+      this.puuid = this.data.puuid
+    }
   }
-}
 </script>
